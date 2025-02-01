be96f20e487ced054827c9dc195a5bb5fc59ace3
Sumitb <138216501+sumitbhot41@users.noreply.github.com>
2025-02-02 00:09:51 +0530

    Updated pipeline with necessary file and requirment txt

---
+++ pythonnew.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import scipy
+import joblib
+import yaml
+import matplotlib
+import seaborn as sns
+import xgboost
+import tqdm
+
+print(f"NumPy Version: {np.__version__}")
+print(f"Pandas Version: {pd.__version__}")
+print(f"Scikit-Learn Version: {sklearn.__version__}")
+print(f"Scipy Version: {scipy.__version__}")
+print(f"Joblib Version: {joblib.__version__}")
+print(f"PyYAML Version: {yaml.__version__}")
+print(f"Matplotlib Version: {matplotlib.__version__}")
+print(f"Seaborn Version: {sns.__version__}")
+print(f"XGBoost Version: {xgboost.__version__}")
+print(f"TQDM Version: {tqdm.__version__}")
